9616566924b6bfa3a1f5637402c5b36247233737
David <87758475+Redseason1@users.noreply.github.com>
2022-04-04 21:29:05 +0800

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 public class GamePanel extends JPanel implements Runnable{
-    static final int GAME_WIDTH = 1000;
+    	static final int GAME_WIDTH = 1000;
 	static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
 	static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH,GAME_HEIGHT);
 	static final int BALL_DIAMETER = 20;
@@ -20,7 +20,7 @@ public class GamePanel extends JPanel implements Runnable{
 	Ball ball;
 	Score score;
 	
-	GamePanel(){
+	public GamePanel(){
 		newPaddles();
 		newBall();
 		score = new Score(GAME_WIDTH,GAME_HEIGHT);
@@ -36,28 +36,33 @@ public class GamePanel extends JPanel implements Runnable{
 		random = new Random();
 		ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),random.nextInt(GAME_HEIGHT-BALL_DIAMETER),BALL_DIAMETER,BALL_DIAMETER);
 	}
+	
 	public void newPaddles() {
 		paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
 		paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
 	}
+	
 	public void paint(Graphics g) {
 		image = createImage(getWidth(),getHeight());
 		graphics = image.getGraphics();
 		draw(graphics);
 		g.drawImage(image,0,0,this);
 	}
+	
 	public void draw(Graphics g) {
 		paddle1.draw(g);
 		paddle2.draw(g);
 		ball.draw(g);
 		score.draw(g);
-        Toolkit.getDefaultToolkit().sync(); // I forgot to add this line of code in the video, it helps with the animation
+        	Toolkit.getDefaultToolkit().sync(); // I forgot to add this line of code in the video, it helps with the animation
 	}
+	
 	public void move() {
 		paddle1.move();
 		paddle2.move();
 		ball.move();
 	}
+	
 	public void checkCollision() {	
 		//bounce ball off top & bottom window edges
 		if(ball.y <=0) {
@@ -110,6 +115,7 @@ public class GamePanel extends JPanel implements Runnable{
 			System.out.println("Player 1: "+score.player1);
 		}
 	}
+	
 	public void run() {
 		//game loop
 		long lastTime = System.nanoTime();
@@ -128,6 +134,7 @@ public class GamePanel extends JPanel implements Runnable{
 			}
 		}
 	}
+	
 	public class AL extends KeyAdapter{
 		public void keyPressed(KeyEvent e) {
 			paddle1.keyPressed(e);
